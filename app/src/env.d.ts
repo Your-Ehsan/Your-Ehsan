d9e3fcab1fd69d97f390d540bfd6bebe397b36db
Your-Ehsan <ehsanshahid787@gmail.com>
2024-02-04 12:53:05 +0500

    firebase initialized

--- app/src/env.d.ts
+++ app/src/env.d.ts
@@ -7,9 +7,21 @@ interface ImportMetaEnv {
   readonly SANITY_DATASET: string
   readonly SANITY_API_VERSION: string
   readonly SANITY_ACCESS_TOKEN: string
+
+  readonly FIREBASE_TYPE: string
+  readonly FIREBASE_PROJECT_ID: string
+  readonly FIREBASE_PRIVATE_KEY_ID: string
+  readonly FIREBASE_PRIVATE_KEY: string
+  readonly FIREBASE_CLIENT_EMAIL: string
+  readonly FIREBASE_CLIENT_ID: string
+  readonly FIREBASE_AUTH_URI: string
+  readonly FIREBASE_TOKEN_URI: string
+  readonly FIREBASE_AUTH_PROVIDER_X509_CERT_URL: string
+  readonly FIREBASE_CLIENT_X509_CERT_URL: string
+  readonly FIREBASE_UNIVERSE_DOMAIN: string
   // more env variables...
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
