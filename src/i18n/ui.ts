70c9ece55d6ab72b5155f7fdf2eb6e9d94f93fd5
Your-Ehsan <ehsanshahid787@gmail.com>
2024-03-20 17:31:54 +0500

    some little code improvements

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,19 +1,27 @@
+// Define available languages and their corresponding names
 export const languages = {
   en: 'English',
   fr: 'Français',
-}
-export const locales = ['en', 'fr']
-export const defaultLang = 'en'
+};
 
+// List of supported locales
+export const locales = ['en', 'fr'];
+
+// Default language
+export const defaultLang = 'en';
+
+// Define UI text for each language
 export const ui = {
+  // English UI text
   en: {
     'nav.home': 'Home',
     'nav.about': 'About',
     'nav.twitter': 'Twitter',
   },
+  // French UI text
   fr: {
     'nav.home': 'Accueil',
     'nav.about': 'À propos',
     'nav.twitter': 'Twitter',
   },
-} as const
+} as const;
